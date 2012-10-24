429f6928deb4532feb61ddfdf125280029222cf0
gianni <gianni.scarnera@epfl.ch>
2012-10-24 17:40:33 +0200

    C'EST MIEU! tjrs quelque probleme avec le blanc et le noir suivant l'angle. le code plagiat en commentaire est à enlevé quand on a finit.

--- Ex3/03_Exc_OpenGL_Framework/src/exercises/01-CubeViewer/cube.fs
+++ Ex3/03_Exc_OpenGL_Framework/src/exercises/01-CubeViewer/cube.fs
@@ -6,11 +6,20 @@ varying vec3 normal;
 void main()
 {
 	// Exercise 3.3.2
-    if(-dot((0.0,0.0,-1.0),normal)>0.0){
-        gl_FragColor = color*(-dot((0.0,0.0,-1.0),normal));
+    vec3 lightVector = vec3(0,0,-1);
+    float dotProduct = -dot(lightVector,normal);
+    vec4 black = (0.0,0.0,0.0,1.0);
+    if(dotProduct > 0.0){
+        gl_FragColor = color*dotProduct;
 	} else {
-        gl_FragColor = (0.0,0.0,0.0,1.0);
+        gl_FragColor = black;
     }
+//    vec3 lightDir = vec3(0,0,-1);
+//    float scale = -dot(lightDir, normal);
+//    if(scale < 0.0)
+//        scale = 0.0;
+//    gl_FragColor.rgb = color.rgb*scale;
+//    gl_FragColor.a = color.a;
 	// need this line so OpenGL doesn't optimize out the variables -- remove in your solution
     // {
 	//normal; color;
