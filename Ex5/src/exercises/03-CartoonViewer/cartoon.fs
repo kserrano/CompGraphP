dccd55f922950b5f8adffab498fd0b704b511179
kserrano <kevin.serrano@epfl.ch>
2012-11-08 09:16:19 +0100

    tool shading

--- Ex5/src/exercises/03-CartoonViewer/cartoon.fs
+++ Ex5/src/exercises/03-CartoonViewer/cartoon.fs
@@ -9,11 +9,13 @@ uniform vec3 diffuseColor;
 void main()
 {	
 	
-	vec3 color =  ( (useTexture > 0) ? diffuseColor*texture2D(textureDiffuse, gl_TexCoord[0].xy).xyz : diffuseColor );
+	//vec3 color =  ( (useTexture > 0) ? diffuseColor*texture2D(textureDiffuse, gl_TexCoord[0].xy).xyz : diffuseColor );
 	
-	vec3 L = vec3(0.0,0.0,1.0);
-	vec3 N = normalize(normal);
+	//vec3 L = vec3(0.0,0.0,1.0);
+	//vec3 N = normalize(normal);
+	//float D = max(dot(N,L),0.0); // 0 < D < 1
+	//color = texture2D(texture,vec2(D,0.0)).xyz*color;
 
-	gl_FragColor = vec4(color, 1.0);
+	gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 	
 }
